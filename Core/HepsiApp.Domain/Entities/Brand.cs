f6018a728e01f3b4710de9d61e53c0dd86b0b6e7
bora399 <borasaltik@hotmail.com>
2025-03-01 17:32:42 +0300

    Some Entities Added, Entity Base Created.

---
+++ Core/HepsiApp.Domain/Entities/Brand.cs
@@ -0,0 +1,14 @@
+﻿using HepsiApp.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApp.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+
+    }
+}
